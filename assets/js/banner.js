e99bec263c05be35467a294be621c704c70b0c0b
wangwh8 <wangwh8@lenovo.com>
2018-04-25 13:30:51 +0800

    new site

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -6,11 +6,11 @@ $(function(){
             images: [
                 "assets/img/lunch_1.jpg",
                 "assets/img/lunch_2.jpg",
-                "assets/img/slider/1.jpg",
-                "assets/img/slider/2.jpg",
-                "assets/img/slider/3.jpg",
-                "assets/img/slider/4.jpg",
-                "assets/img/slider/5.jpg",
+                "http://i2.bvimg.com/642349/284476fd0da91d0a.jpg",
+                "http://i2.bvimg.com/642349/bf5d16434897b6d1.jpg",
+                "http://i2.bvimg.com/642349/e4afe4ff4b43bcf1.jpg",
+                "http://i2.bvimg.com/642349/e7f50c4a86c84aa9.jpg",
+                "http://i2.bvimg.com/642349/fa31a3708ba844a9.jpg",
             ]
         },
         buttonSlot: {
